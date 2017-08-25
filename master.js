6031d1b26546f101dfc510c367a5e42cc245b4c7
Animesh Saraswat <animesh.s0211@gmail.com>
2017-08-25 18:48:09 +0530

    initial push

---
+++ master.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if(document.cookie != 0) {
+    incrementCookie();
+  }
+  else {
+    setCookie();
+  }
+});
+
+function incrementCookie() {
+  var cookieFeatch = document.cookie.split("=");
+  console.log(cookieFeatch);
+  var visitCount = Number(cookieFeatch[1]);
+  visitCount += 1;
+  var lastDate = new Date();
+  document.cookie = 'counter='+visitCount + '=' + 'lastVisit=' + lastDate.toDateString() + "=" + lastDate.toTimeString();
+  document.getElementById('printCounter').innerHTML = "You have visited " + visitCount + " number of times";
+  document.getElementById('printLastVisit').innerHTML = "You last visited on " + cookieFeatch[3] + " at " + cookieFeatch[4];
+
+};
+
+function setCookie() {
+  var visitCount = 1;
+  var lastDate = new Date();
+  var expTime = lastDate + 1000*36000;
+  document.cookie = "counter="+ visitCount + "=" + "lastVisit=" + lastDate.toDateString() + "=" + lastDate.toTimeString();
+  document.getElementById('printCounter').innerHTML = "You have never visited before";
+  document.getElementById('printLastVisit').innerHTML = "This is your first time";
+};
